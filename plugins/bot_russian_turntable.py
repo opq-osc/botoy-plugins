424c6a9459ecea4afdb33abd694cb5ad6bd9c883
wongxy <xiyao.wong@foxmail.com>
2021-07-25 21:44:13 +0800

    style(russian_turntable): format

--- plugins/bot_russian_turntable.py
+++ plugins/bot_russian_turntable.py
@@ -15,43 +15,43 @@ def reload(count=1) -> List[int]:
 
 
 turntable = SessionHandler(
-    ignore_botself, equal_content('开始轮盘'), single_user=False
+    ignore_botself, equal_content("开始轮盘"), single_user=False
 ).receive_group_msg()
 
 
 @turntable.handle
 def _():
     count_input: str = session.want(  # type:ignore
-        'count', '俄罗斯轮盘游戏开始，请发送要填充的子弹数（大于0，小于6）', default='1', timeout=20
+        "count", "俄罗斯轮盘游戏开始，请发送要填充的子弹数（大于0，小于6）", default="1", timeout=20
     )
     if count_input.isdigit():
         count = int(count_input)
         if count < 0 or count >= 6:
-            turntable.finish('输入不合法，请重新开始')
+            turntable.finish("输入不合法，请重新开始")
     else:
         count = 1
-    session.send_text(f'装填子弹：{count}颗\n\n(任意用户发送任意内容开枪, 30s无人回复将自动结束游戏) ')
+    session.send_text(f"装填子弹：{count}颗\n\n(任意用户发送任意内容开枪, 30s无人回复将自动结束游戏) ")
     clip = reload(count)
     while clip:
         # 剩下全是空弹或实弹就没必要继续了
         if set(clip) == {1}:
-            turntable.finish('剩余全是实弹, 游戏结束')
+            turntable.finish("剩余全是实弹, 游戏结束")
         elif set(clip) == {0}:
-            turntable.finish('剩余全是空弹, 游戏结束')
+            turntable.finish("剩余全是空弹, 游戏结束")
 
         shot = session.want(
-            'shot',
-            f'当前剩余枪数: {len(clip)}. ',
+            "shot",
+            f"当前剩余枪数: {len(clip)}. ",
             pop=True,
             timeout=30,
         )
         if shot is None:
-            turntable.finish('无人回复，游戏自动结束')
+            turntable.finish("无人回复，游戏自动结束")
 
         if clip.pop() == 1:
-            msg = '很不幸，你中枪了:('
+            msg = "很不幸，你中枪了:("
         else:
-            msg = '恭喜你，是空弹!'
+            msg = "恭喜你，是空弹!"
         session.send_text(msg)
 
     turntable.finish()
