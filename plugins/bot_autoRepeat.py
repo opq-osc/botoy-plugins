202629997dde6a1f7f6c88e9b5f4c6e4828a6dfe
wongxy <xiyao.wong@foxmail.com>
2021-07-25 21:35:13 +0800

    fix(autoReqpeat): 避免在同一语境下，同样的内容反复加一

--- plugins/bot_autoRepeat.py
+++ plugins/bot_autoRepeat.py
@@ -1,11 +1,11 @@
 from collections import defaultdict
-from queue import deque  # type:ignore
+from queue import deque  # type: ignore
 from threading import Lock
 
 from botoy import GroupMsg
 from botoy import decorators as deco
 from botoy.collection import MsgTypes
-from botoy.parser import group as gp
+from botoy.refine import _PicGroupMsg, refine_pic_group_msg
 from botoy.sugar import Picture, Text
 
 # 自动消息加一功能, 支持文字消息和图片消息
@@ -14,6 +14,7 @@ from botoy.sugar import Picture, Text
 class RepeatDeque(deque):
     def __init__(self, *args, **kwargs):
         super().__init__(maxlen=3, *args, **kwargs)
+        self.last = None
         self.lock = Lock()
         with self.lock:
             self.refresh()
@@ -23,12 +24,18 @@ class RepeatDeque(deque):
             self.append(i)
 
     def should_repeat(self, item):
+        ret = False
         with self.lock:
+            if item != self.last:
+                self.last = None
+
             self.append(item)
             if len(set(self)) == 1:
                 self.refresh()
-                return True
-        return False
+                if item != self.last:
+                    ret = True
+                self.last = item
+        return ret
 
 
 text_deque_dict = defaultdict(RepeatDeque)
@@ -43,8 +50,9 @@ def receive_group_msg(ctx: GroupMsg):
         if text_deque_dict[ctx.FromGroupId].should_repeat(text):
             Text(text)
     elif ctx.MsgType == MsgTypes.PicMsg:
-        pic_data = gp.pic(ctx)
-        if pic_data:
-            pic_md5 = pic_data.GroupPic[0].FileMd5
-            if pic_deque_dict[ctx.FromGroupId].should_repeat(pic_md5):
-                Picture(pic_md5=pic_md5)
+        pic_ctx: _PicGroupMsg = refine_pic_group_msg(ctx)
+        if pic_ctx is not None:
+            pics = pic_ctx.GroupPic
+            pic = pics[0]
+            if pic_deque_dict[ctx.FromGroupId].should_repeat(pic.FileMd5):
+                Picture(pic_md5=pic.FileMd5)
