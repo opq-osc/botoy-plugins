16fea46eb9951b4551d24029434e2818aaca5f07
wongxy <xiyao.wong@foxmail.com>
2021-08-06 14:37:06 +0800

    feat(kiss): 支持自定义图片

--- plugins/bot_kiss_gif/__init__.py
+++ plugins/bot_kiss_gif/__init__.py
@@ -1,6 +1,8 @@
 '''制作亲亲表情包
 1. AT两个人，并发送kiss
 2. AT一个人发送kiss
+3. At一个人加一张图和kiss
+4. 发送两张图和kiss
 提示：将kiss改为kissR可切换亲和被亲的对象
 '''
 from botoy import GroupMsg, S
@@ -15,19 +17,36 @@ def receive_group_msg(ctx: GroupMsg):
     o = t = None
 
     if ctx.MsgType == 'PicMsg':
-        # TODO: 允许发送图片配合AT来制作，需要botoy完善图片消息解析, 因为即使AT了
-        # 只要发送了图片就是图片消息：(
-        pass
-
+        pic = gp.pic(ctx)
+        if not pic:
+            return
+        if not 'kiss' in pic.Content:
+            return
+        # 2 张图
+        if len(pic.GroupPic) >= 2:
+            o, t = pic.GroupPic[0].Url, pic.GroupPic[1].Url
+        # 1张图，没有at就是发送人
+        else:
+            t = pic.GroupPic[0].Url
+            # 有at
+            if pic.UserExt:
+                o = pic.UserExt[0].QQUid
+            # 无 at
+            else:
+                o = ctx.FromUserId
+        if 'kissR' in pic.Content:
+            o, t = t, o
     elif ctx.MsgType == 'AtMsg':
         at = gp.at(ctx)
         if not at:
             return
         if not 'kiss' in at.Content:
             return
+        # at 两个人
         if len(at.UserID) >= 2:
             o, t = at.UserID[:2]
         else:
+            # at 一个人
             o = ctx.FromUserId
             t = at.UserID[0]
 
