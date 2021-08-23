3d1e275b407174e6fa7a65a55fc5082d9c02aeb1
wongxy <xiyao.wong@foxmail.com>
2021-08-23 14:02:43 +0800

    自定义图片：使用统一的下载函数

--- plugins/bot_custom_image/config.py
+++ plugins/bot_custom_image/config.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from botoy import jconfig
-from botoy.contrib import get_cache_dir
+from botoy.contrib import download, get_cache_dir
 
 
 def get_config(key, default=None):
@@ -29,30 +29,10 @@ else:
 FONT_PATH = CACHE_PATH / font_name
 
 if not FONT_PATH.exists():
-    print("custom image 插件: 下载字体中....")
-    import os
-
-    import httpx
-
-    try:
-        with httpx.stream(
-            "GET",
-            f"https://github.com/opq-osc/botoy-plugins/releases/download/simhei-fonts/{font_name}",
-        ) as resp:
-            print("连接字体资源成功")
-            total_size = int(resp.headers["content-length"])
-            downloaded_size = 0
-            with open(FONT_PATH, "wb") as f:
-                for chunk in resp.iter_bytes(1024):
-                    percent = int(100 * downloaded_size / total_size)
-                    print("\r|{:100}|{}%".format("#" * percent, percent), end="")
-                    f.write(chunk)
-                    downloaded_size += 1024
-        print("\n下载字体完成")
-    except Exception:
-        if FONT_PATH.exists():
-            os.remove(FONT_PATH)
-        raise
+    download(
+        f"https://github.com/opq-osc/botoy-plugins/releases/download/simhei-fonts/{font_name}",
+        FONT_PATH,
+    )
 
 QRCODE_PATH = CACHE_PATH / "list-qr.jpg"
 NAMES_PATH = IMAGES_PATH / "names.json"
